805b05b2842593d9f2987923d727579b14f2ad0a
Harin3Bone <harin.thananam@gmail.com>
2021-03-08 22:01:09 +0700

    Initial - Add SCB Project

---
+++ SCB/src/test/java/com/developer/scb/ScbDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.scb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScbDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
